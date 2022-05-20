2e2964334bbce63b8a8e8f4420b84ad83fece018
William Powell <William.Powell@abdynamics.com>
2022-05-20 14:35:13 +0100

    fixed driver issue

--- webscrape.py
+++ webscrape.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# sudo apt install chromium-chromedriver
+# sudo apt install chromium-browser
 
 class Webscrape:
   def __init__(self, option=''):
     chromeOptions = Options()
     if option != 'debug':
       chromeOptions.add_argument('--headless')
-    self.driver = webdriver(executable_path = '/usr/lib/chromium-browser/chromedriver', options=chromeOptions)
+    self.driver = webdriver.Chrome(executable_path = '/usr/lib/chromium-browser/chromedriver', options=chromeOptions)
   
   def checkPage(self, websiteURL, keyword):
     self.driver.get(websiteURL)
